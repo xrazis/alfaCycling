9cfff302dd804799dc8e8d58c47f54c2db734c6c
Haris Razis <dev@xrazis.com>
2020-10-23 12:19:16 +0300

    🔨 Change config to detect NODE_ENV

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+    session_secret: process.env.SESSION_SECRET,
+    db_host: process.env.DB_HOST,
+    db_user: process.env.DB_USER,
+    db_password: process.env.DB_PASSWORD,
+    db_port: process.env.DB_PORT,
+    db_database: process.env.DB_DATABASE,
+    email_host: process.env.EMAIL_HOST,
+    email_user: process.env.EMAIL_USER,
+    email_password: process.env.EMAIL_PASSWORD,
+    tiny_api: process.env.TINY_API,
+    admin_code: process.env.ADMIN_CODE
+};
